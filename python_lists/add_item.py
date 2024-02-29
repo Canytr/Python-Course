929f808e5b845c106ae4246a2728b38632ec8398
Can YETER <cannytr@gmail.com>
2024-02-29 14:25:26 +0300

    added python_lists

---
+++ python_lists/add_item.py
@@ -0,0 +1,6 @@
+#add an item to the end of a list
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+
+print(thislist)
---
+++ python_lists/add_item_specified.py
@@ -0,0 +1,5 @@
+#add an item at a specified index
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
