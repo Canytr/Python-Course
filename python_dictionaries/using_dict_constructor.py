6786cc48185ed90054f7e84eff3d5fdbb3b04b9f
Can YETER <cannytr@gmail.com>
2024-03-06 14:00:47 +0300

    update python_dictionaries

---
+++ python_dictionaries/using_dict_constructor.py
@@ -0,0 +1,6 @@
+thisdict =dict(brand="Ford", model="Mustang", year=1964)
+
+print(thisdict)
+
+# note that keywords are not string literals
+# note the use of equals rather than colon for the assignment
