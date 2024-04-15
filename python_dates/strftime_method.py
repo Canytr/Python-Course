35800bd6a296f3498c559cba0992645090636255
Can YETER <cannytr@gmail.com>
2024-04-15 13:40:01 +0300

    added strftime method

---
+++ python_dates/strftime_method.py
@@ -0,0 +1,10 @@
+import datetime
+
+x = datetime.datetime(2018, 6, 1)
+
+print(x.strftime("%A")) # Friday
+print(x.strftime("%B")) # June
+print(x.strftime("%C")) # 20
+print(x.strftime("%D")) # 06/01/18
+print(x.strftime("%F")) # 2018-06-01
+print(x.strftime("%G")) # 2018
